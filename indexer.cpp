fbd3cf646284801e2e76314dd61971f1c48bd20d
Laughlin Dawes <ldawes@gmail.com>
2017-05-10 18:13:08 +1000

    started on merge

--- indexer.cpp
+++ indexer.cpp
@@ -26,8 +26,159 @@ void indexer::add_file(const string &file) {
 
 // finalise index
 void indexer::finalise() {
+  // if we have not yet written to file, can do so now
+  if (postfilenum == 0) {
+    // first the dictionary
+    string indexpath = index_dir + DICTIONARY_FILE_NAME + INDEX_SUFFIX;
+    ofstream dict(indexpath.c_str(), ofstream::out);
+    if (!dict) {
+      cerr << "Cannot open " << indexpath << " for writing" << endl;
+      exit(1);
+    }
+    for (auto it = tokens.begin(); it != tokens.end(); ++it) {
+      dict << it->first << endl;
+    }
+    dict.close();
+
+    // now the posting pointer and doc freq
+    // NB could shorten this as doc freq is a function of posting pointer
+    indexpath = index_dir + POINTER_DOC_FREQ_FILE_NAME + INDEX_SUFFIX;
+    ofstream point_freq_file(indexpath.c_str(), ofstream::out);
+    uint32_t pos = 0;
+    unsigned posting_element_size = sizeof tokens[0].first[0];
+    for (auto it = tokens.begin(); it != tokens.end(); ++it) {
+      point_freq_file.write(reinterpret_cast<char *>(&pos), sizeof(pos));
+      uint16_t freq = it->second.first.size();
+      pos += posting_element_size * freq; 
+      point_freq_file.write(reinterpret_cast<char *>(&freq), sizeof(freq));
+    }
+    point_freq_file.close();
+
+    // now the postings list
+    indexpath = index_dir + POSTINGS_FILE_NAME + INDEX_SUFFIX;
+    ofstream posting_file(indexpath.c_str(), ofstream::out);
+    for (auto it = tokens.begin(); it != tokens.end(); ++it) {
+      for (auto pit = it->second.first.begin(); pit != it->second.first.end();
+        ++pit) {
+        posting_file.write(reinterpret_cast<char *>(&(*pit)), sizeof(*pit));
+      }
+    }
+    posting_file.close();
+
+    // and the frequency list
+    indexpath = index_dir + FREQUENCY_FILE_NAME + INDEX_SUFFIX;
+    ofstream frequency_file(indexpath.c_str(), ofstream::out);
+    for (auto it = tokens.begin(); it != tokens.end(); ++it) {
+      for (auto fit = it->second.second.begin(); fit != it->second.second.end();
+        ++fit) {
+        frequency_file.write(reinterpret_cast<char *>(&(*fit)), sizeof(*fit));
+      }
+    }
+    frequency_file.close();
+  }
+
+  // we have multiple files to deal with, and need to merge them
+  else {
+    // write remaining data
+    flush_to_file();
+    // merge the first two indices to a temporary index
+    index_merge("0", "1", "temp0");
+
+    // now merge the remaining indices into the temporary indices
+    int tempnum = 1;
+    while (tempnum < postfilenum - 2) {
+      index_merge("temp" + to_string(tempnum - 1), tempnum + 1, 
+        "temp" + to_string(tempnum));
+      ++tempnum;
+    }
+
+    // now merge the last temp into the final index
+    index_merge("temp" + to_string(tempnum - 1), tempnum + 1, INDEX_SUFFIX);
+  }
+}
+
+void indexer::index_merge(const &string a, const &string b, const &string out) {
+  // open dictionary files
+  ifstream a_dictfile, b_dictfile;
+  ofstrem out_dictfile;
+  string dict_path = index_dir + DICTIONARY_FILE_NAME;
+  a_dictfile.open((dict_path + a).c_str(), ifstream::in);
+  b_dictfile.open((dict_path + b).c_str(), ifstream::in);
+  out_dictfile.open((dict_path + out).c_str(), ofstream::out);
+
+  // open pointer/frequency files
+  ifstream a_pfile, b_pfile;
+  ofstrem out_pfile;
+  string p_path = index_dir + POINTER_DOC_FREQ_FILE_NAME;
+  a_pfile.open((p_path + a).c_str(), ifstream::in);
+  b_pfile.open((p_path + b).c_str(), ifstream::in);
+  out_pfile.open((p_path + out).c_str(), ofstream::out);
+
+  // open postings files
+  ifstream a_postfile, b_postfile;
+  ofstream out_postfile;
+  string post_path = index_dir + POSTINGS_FILE_NAME;
+  a_postfile.open((post_path + a).c_str(), ifstream::in);
+  b_postfile.open((post_path + b).c_str(), ifstream::in);
+  out_postfile.open((post_path + out).c_str(), ofstream::out);
+
+  // open frequency files
+  ifstream a_freqfile, b_freqfile;
+  ofstrem out_freqfile;
+  string freq_path = index_dir + FREQUENCY_FILE_NAME;
+  a_freqfile.open((freq_path + a).c_str(), ifstream::in);
+  b_freqfile.open((freq_path + b).c_str(), ifstream::in);
+  out_freqfile.open((freq_path + out).c_str(), ofstream::out);
+
+  // read dictionary files one word at a time
+  string next_a, next_b;
+  a_dictfile >> next_a;
+  b_dictfile >> next_b;
+  bool a_eof = false;
+  bool b_eof = false;
+  while (!a_eof && !b_eof) {
+    if (a_eof) {
+      //TODO insert b
+      b_dictfile >> next_b;
+      if (!b_dictfile) b_eof = true;
+      continue;
+    }
+    if (b_eof) {
+      //TODO insert a
+      a_dictfile >> next_a;
+      if (!a_dictfile) a_eof = true;
+      continue;
+    }
+    int compare = next_a.compare(next_b);
+    if (compare == 0) {
+      //TODO merge those two words
+      a_dictfile >> next_a;
+      b_dictfile >> next_b;
+      if (!a_dictfile) a_eof = true;
+      if (!b_dictfile) b_eof = true;
+    }
+    else if (compare < 0) {
+      // a is smaller than b, insert a
+      //TODO insert a
+      a_dictfile >> next_a;
+      if (!a_dictfile) a_eof = true;
+      continue;
+    }
+    else {
+      //TODO insert b
+      b_dictfile >> next_b;
+      if (!b_dictfile) b_eof = true;
+    }
+  }
+}
+
+void indexer::flush_to_file() {
+
+  cout << "flushing to file " << 
+  POSTINGS_FILE_NAME + to_string(postfilenum) + INDEX_SUFFIX << endl;
+
   // first the dictionary
-  string indexpath = index_dir + DICTIONARY_FILE_NAME + INDEX_SUFFIX;
+  string indexpath = index_dir + DICTIONARY_FILE_NAME + to_string(postfilenum);
   ofstream dict(indexpath.c_str(), ofstream::out);
   if (!dict) {
     cerr << "Cannot open " << indexpath << " for writing" << endl;
@@ -39,8 +190,7 @@ void indexer::finalise() {
   dict.close();
 
   // now the posting pointer and doc freq
-  // NB could shorten this as doc freq is a function of posting pointer
-  indexpath = index_dir + POINTER_DOC_FREQ_FILE_NAME + INDEX_SUFFIX;
+  /*indexpath = index_dir + POINTER_DOC_FREQ_FILE_NAME + to_string(postfilenum);
   ofstream point_freq_file(indexpath.c_str(), ofstream::out);
   uint32_t pos = 0;
   unsigned posting_element_size = sizeof tokens[0].first[0];
@@ -50,10 +200,10 @@ void indexer::finalise() {
     pos += posting_element_size * freq; 
     point_freq_file.write(reinterpret_cast<char *>(&freq), sizeof(freq));
   }
-  point_freq_file.close();
+  point_freq_file.close();*/
 
   // now the postings list
-  indexpath = index_dir + POSTINGS_FILE_NAME + INDEX_SUFFIX;
+  indexpath = index_dir + POSTINGS_FILE_NAME + to_string(postfilenum);
   ofstream posting_file(indexpath.c_str(), ofstream::out);
   for (auto it = tokens.begin(); it != tokens.end(); ++it) {
     for (auto pit = it->second.first.begin(); pit != it->second.first.end();
@@ -64,7 +214,7 @@ void indexer::finalise() {
   posting_file.close();
 
   // and the frequency list
-  indexpath = index_dir + FREQUENCY_FILE_NAME + INDEX_SUFFIX;
+  indexpath = index_dir + FREQUENCY_FILE_NAME + to_string(postfilenum);
   ofstream frequency_file(indexpath.c_str(), ofstream::out);
   for (auto it = tokens.begin(); it != tokens.end(); ++it) {
     for (auto fit = it->second.second.begin(); fit != it->second.second.end();
@@ -73,14 +223,12 @@ void indexer::finalise() {
     }
   }
   frequency_file.close();
-}
 
-void indexer::flush_to_file() {
-  // TODO implement
-  /*cout << "flushing to file " << 
-    POSTINGS_FILE_NAME + to_string(postfilenum) + INDEX_SUFFIX << endl;
+  // free up all that lovely memory
+  tokens.clear();
+
   ++postfilenum;
-  postings = 0; */
+  postings = 0;
 }
 
 // set index directory and make sure it exists
